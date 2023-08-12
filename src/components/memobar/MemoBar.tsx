6b509ad35c23f1c80cdcc56b8ad536d2b743c700
Timeuh <timeuuh@gmail.com>
2023-08-12 17:18:17 +0200

    feat: use memo array object for memo display

--- src/components/memobar/MemoBar.tsx
+++ src/components/memobar/MemoBar.tsx
@@ -1,23 +1,44 @@
 import MemoBarItem from '@/components/memobar/MemoBarItem';
+import {Memo} from '@/appTypes';
 
 export default function MemoBar() {
+  const memos: Array<Memo> = [
+    {
+      id: 1,
+      title: 'Premier Memo',
+      content: 'Contenu du premier memo'
+    },
+    {
+      id: 2,
+      title: 'Second Memo',
+      content: 'Contenu du second memo'
+    },
+    {
+      id: 3,
+      title: 'Troisième Memo',
+      content: 'Contenu du troisième memo'
+    },
+    {
+      id: 4,
+      title: 'Quatrième Memo',
+      content: 'Contenu du quatrième memo'
+    },
+    {
+      id: 5,
+      title: 'Cinquième Memo',
+      content: 'Contenu du cinquième memo'
+    }
+  ];
 
   return (
     <div id={'memoBar'} className={'memo-bar-desktop'}>
       <h1 className={'text-xl font-bold border-b-2 border-dark h-12 text-center w-5/6 pt-4'}>Vos mémos</h1>
       <div id={'memoContainer'} className={'flex flex-col items-center space-y-4 w-full h-full pt-4 overflow-y-scroll'}>
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
-        <MemoBarItem />
+        {
+          memos.map((memo) => {
+            return <MemoBarItem key={memo.id} memo={memo} />;
+          })
+        }
       </div>
     </div>
   );

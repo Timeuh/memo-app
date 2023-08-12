e2662ba1f96e6cff8bbb22e35dfeebf6dc765b10
Timeuh <timeuuh@gmail.com>
2023-08-12 21:32:51 +0200

    feat: set currentId in local storage in case this is the first launch

--- src/hooks/useMemoList.ts
+++ src/hooks/useMemoList.ts
@@ -4,6 +4,11 @@ import {Memo} from '@/appTypes';
 const useMemoList = (setMemos: React.Dispatch<React.SetStateAction<Memo[]>>) => {
   useEffect(() => {
     const storedMemos = localStorage.getItem('memos');
+    const doesIdExist = localStorage.getItem('currentId') !== null;
+
+    if (!doesIdExist) {
+      localStorage.setItem('currentId', '0');
+    }
 
     if (storedMemos !== null){
       setMemos(JSON.parse(storedMemos));

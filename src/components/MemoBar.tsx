714ef57e28cf88bec6a43977a2c94920718d6ee7
Timeuh <timeuuh@gmail.com>
2023-08-12 16:45:07 +0200

    style: hide desktop memo bar on mobile

--- src/components/MemoBar.tsx
+++ src/components/MemoBar.tsx
@@ -1,6 +1,6 @@
 export default function MemoBar() {
   return (
-    <div id={'memoBar'} className={'h-4/6 w-1/6 bg-violet absolute left-10 shadow-2xl shadow-main rounded-2xl p-2 flex flex-col items-center'}>
+    <div id={'memoBar'} className={'memo-bar-desktop'}>
       <h1 className={'text-xl font-bold border-b-2 border-dark h-12 text-center w-5/6 pt-4'}>Vos mémos</h1>
     </div>
   );

aca2b505b881ec83aa46e7de68a6b9ea583f016f
Timeuh <timeuuh@gmail.com>
2023-08-12 17:27:40 +0200

    feat: pass memo array to memo bar

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,39 @@ export default function App() {
     content: ''
   };
 
+  const memos: Array<Memo> = [
+    {
+      id: 1,
+      title: 'Premier Memo',
+      content: 'Contenu du premier memo'
+    },
+    {
+      id: 2,
+      title: 'Second Memo',
+      content: 'Contenu du second memo'
+    },
+    {
+      id: 3,
+      title: 'Troisième Memo',
+      content: 'Contenu du troisième memo'
+    },
+    {
+      id: 4,
+      title: 'Quatrième Memo',
+      content: 'Contenu du quatrième memo'
+    },
+    {
+      id: 5,
+      title: 'Cinquième Memo',
+      content: 'Contenu du cinquième memo'
+    }
+  ];
+
   return (
     <div id='App' className={'w-full h-screen bg-light flex flex-col items-center justify-center space-y-24 font-SourceCodePro'}>
       <h1 className={'app-title'}>Créez vos mémos</h1>
       <MemoForm memo={placeholderMemo}/>
-      <MemoBar />
+      <MemoBar memos={memos}/>
     </div>
   );
 }

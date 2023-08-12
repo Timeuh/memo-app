0b5e27ced8b2a9dbb5adf948a06773acf22bb734
Timeuh <timeuuh@gmail.com>
2023-08-12 17:51:48 +0200

    feat: use state for placeholder memo + create placeholder memo fields change method

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import MemoForm from '@/components/MemoForm';
 import MemoBar from '@/components/memobar/MemoBar';
 import {Memo} from '@/appTypes';
+import {useState} from 'react';
 
 export default function App() {
-  const placeholderMemo: Memo = {
-    id: 0,
-    title: '',
-    content: ''
-  };
+  const [placeholderMemo, setPlaceholderMemo] = useState<Memo>(
+    {
+      id: 0,
+      title: '',
+      content: ''
+    }
+  ) ;
 
   const memos: Array<Memo> = [
     {
@@ -41,10 +44,14 @@ export default function App() {
     memos.push(memo);
   };
 
+  const changePlaceholderMemo = (memo: Memo) => {
+    setPlaceholderMemo(memo);
+  };
+
   return (
     <div id='App' className={'w-full h-screen bg-light flex flex-col items-center justify-center space-y-24 font-SourceCodePro'}>
       <h1 className={'app-title'}>Créez vos mémos</h1>
-      <MemoForm memo={placeholderMemo} addMemo={addMemo}/>
+      <MemoForm memo={placeholderMemo} addMemo={addMemo} changeMemo={changePlaceholderMemo}/>
       <MemoBar memos={memos}/>
     </div>
   );

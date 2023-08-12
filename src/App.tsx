ac5ea2afcfa0272588d692c3cde04f20f93f696a
Timeuh <timeuuh@gmail.com>
2023-08-12 17:23:32 +0200

    feat: use placeholder memo for memo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import MemoForm from '@/components/MemoForm';
 import MemoBar from '@/components/memobar/MemoBar';
+import {Memo} from '@/appTypes';
 
 export default function App() {
+  const placeholderMemo: Memo = {
+    id: 0,
+    title: '',
+    content: ''
+  };
+
   return (
     <div id='App' className={'w-full h-screen bg-light flex flex-col items-center justify-center space-y-24 font-SourceCodePro'}>
       <h1 className={'app-title'}>Créez vos mémos</h1>
-      <MemoForm />
+      <MemoForm memo={placeholderMemo}/>
       <MemoBar />
     </div>
   );

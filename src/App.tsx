8d42f44bc29b4be17f9d315d27bb55b78ff37c1e
Timeuh <timeuuh@gmail.com>
2023-08-12 15:03:50 +0200

    style: style app title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import MemoForm from '@/components/MemoForm';
 
 export default function App() {
   return (
-    <div id='App' className={'w-full h-screen bg-light flex flex-col items-center'}>
-      <h1 className={'pt-10 text-4xl text-dark font-bold'}>Créez vos mémos</h1>
+    <div id='App' className={'w-full h-screen bg-light flex flex-col items-center justify-center space-y-24 font-SourceCodePro'}>
+      <h1 className={'app-title'}>Créez vos mémos</h1>
       <MemoForm />
     </div>
   );

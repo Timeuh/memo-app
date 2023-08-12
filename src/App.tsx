3e1159dcc893b337a3bce2fd6eab468abfe12817
Timeuh <timeuuh@gmail.com>
2023-08-12 13:12:01 +0200

    feat: use MemoForm component in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import MemoForm from '@/components/MemoForm';
+
 export default function App() {
   return (
     <div id='App' className={'w-full h-screen bg-light flex flex-col items-center'}>
       <h1 className={'pt-10 text-4xl text-dark font-bold'}>Créez vos mémos</h1>
+      <MemoForm />
     </div>
   );
 }

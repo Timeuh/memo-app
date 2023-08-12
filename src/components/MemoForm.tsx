951b5fcc582727849ac9c444a38723c73ee02101
Timeuh <timeuuh@gmail.com>
2023-08-12 17:23:18 +0200

    feat: use memo item values for display

--- src/components/MemoForm.tsx
+++ src/components/MemoForm.tsx
@@ -1,9 +1,15 @@
-export default function MemoForm() {
+import {Memo} from '@/appTypes';
+
+type Props = {
+  memo: Memo
+}
+
+export default function MemoForm({memo}: Props) {
   return (
     <div id={'memoForm'} className={'h-1/2 xl:w-1/3 flex flex-col items-center justify-center w-4/5'}>
-      <form action='.' className={'memo-form'}>
-        <input id={'memo-title'} type='text' placeholder={'Titre'} className={'memo-input h-10 rounded-full'}/>
-        <textarea id={'memo-content'} placeholder={'Mémo'} className={'memo-input h-1/2 rounded-lg py-2'}/>
+      <form action='.' className={'memo-form'} id={`memo-${memo.id}`}>
+        <input id={'memo-title'} type='text' placeholder={'Titre'} className={'memo-input h-10 rounded-full'} value={memo.title}/>
+        <textarea id={'memo-content'} placeholder={'Mémo'} className={'memo-input h-1/2 rounded-lg py-2'} value={memo.content}/>
         <button id={'memo-submit'} type={'submit'} className={'w-5/6 h-10 bg-dark text-light rounded-full text-xl shadow-2xl'}>Créer</button>
       </form>
     </div>

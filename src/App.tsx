a5fd4d9a262dffcb2d4cd5edda86fe1422de31ba
Timeuh <timeuuh@gmail.com>
2023-08-12 16:03:56 +0200

    feat: use memo bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MemoForm from '@/components/MemoForm';
+import MemoBar from '@/components/MemoBar';
 
 export default function App() {
   return (
     <div id='App' className={'w-full h-screen bg-light flex flex-col items-center justify-center space-y-24 font-SourceCodePro'}>
       <h1 className={'app-title'}>Créez vos mémos</h1>
       <MemoForm />
+      <MemoBar />
     </div>
   );
 }

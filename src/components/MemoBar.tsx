b47e86a9beb26f2ef87e48a9b4fecaaef64b91bd
Timeuh <timeuuh@gmail.com>
2023-08-12 17:07:42 +0200

    feat: use placeholders memo bar item

--- src/components/MemoBar.tsx
+++ src/components/MemoBar.tsx
@@ -1,7 +1,24 @@
+import MemoBarItem from '@/components/MemoBarItem';
+
 export default function MemoBar() {
+
   return (
     <div id={'memoBar'} className={'memo-bar-desktop'}>
       <h1 className={'text-xl font-bold border-b-2 border-dark h-12 text-center w-5/6 pt-4'}>Vos mémos</h1>
+      <div id={'memoContainer'} className={'flex flex-col items-center space-y-4 w-full h-full pt-4 overflow-y-scroll'}>
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+        <MemoBarItem />
+      </div>
     </div>
   );
 }

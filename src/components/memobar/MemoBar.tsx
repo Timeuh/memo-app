ff58f970c3376d5c9909c2233c8d0a59096a690c
Timeuh <timeuuh@gmail.com>
2023-08-12 22:07:01 +0200

    fix: display even when there is no memo

--- src/components/memobar/MemoBar.tsx
+++ src/components/memobar/MemoBar.tsx
@@ -13,7 +13,8 @@ export default function MemoBar({memos, selectMemo}: Props) {
       <h1 className={'text-xl font-bold border-b-2 border-dark h-12 text-center w-5/6 pt-4'}>Vos mémos</h1>
       <div id={'memoContainer'} className={'flex flex-col items-center space-y-4 w-full h-full pt-4 overflow-y-scroll'}>
         {
-          memos.map((memo) => {
+          memos.map((memo: Memo) => {
+            if (!memo) return;
             return <MemoBarItem key={memo.id} memo={memo} selectMemo={selectMemo}/>;
           })
         }

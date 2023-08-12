6fc6d6929ee74d34019221c0bb9dd2050bb9f9c7
Timeuh <timeuuh@gmail.com>
2023-08-12 11:52:01 +0200

    feat: create app base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div id='App'>
-
+    <div id='App' className={'w-full h-screen bg-light flex flex-col items-center'}>
+      <h1 className={'pt-10 text-4xl text-dark font-bold'}>Créez vos mémos</h1>
     </div>
   );
 }

24d1693c88e5dfe217c9cb18efb7f50f23927262
Timeuh <timeuuh@gmail.com>
2023-08-13 11:16:53 +0200

    style: hide options in mobile

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -4,7 +4,7 @@ export default function Options() {
   const {isCurrentLight, setTheme} = useTheme();
 
   return (
-    <div id={'options'} className={`absolute w-1/6 h-24 -top-10 left-10 rounded-md shadow-lg 
+    <div id={'options'} className={`absolute w-1/6 h-24 -top-10 left-10 rounded-md shadow-lg hidden xl:block
       ${isCurrentLight ? 'shadow-main bg-violet' : 'shadow-purple bg-purple'}`}>
       <div className={'h-full flex flex-row items-center justify-center space-x-8'}>
         <button onClick={() => {

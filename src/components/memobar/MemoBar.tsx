f9a29674b2e85973fb251b3f4faf42245228eaae
Timeuh <timeuuh@gmail.com>
2023-08-13 10:13:16 +0200

    style: use theme for memo bar desktop

--- src/components/memobar/MemoBar.tsx
+++ src/components/memobar/MemoBar.tsx
@@ -1,5 +1,6 @@
 import MemoBarItem from '@/components/memobar/MemoBarItem';
 import {Memo} from '@/appTypes';
+import useTheme from '@/hooks/useTheme';
 
 type Props = {
   memos: Array<Memo>,
@@ -7,10 +8,12 @@ type Props = {
 }
 
 export default function MemoBar({memos, selectMemo}: Props) {
+  const {isCurrentLight} = useTheme();
 
   return (
-    <div id={'memoBar'} className={'memo-bar-desktop'}>
-      <h1 className={'text-xl font-bold border-b-2 border-dark h-12 text-center w-5/6 pt-4'}>Vos mémos</h1>
+    <div id={'memoBar'} className={`memo-bar-desktop ${isCurrentLight ? 'bg-violet shadow-main' : 'bg-purple shadow-dark'}`}>
+      <h1 className={`text-xl font-bold border-b-2 h-12 text-center w-5/6 pt-4 
+        ${isCurrentLight ? 'border-dark' : 'border-light text-light'}`}>Vos mémos</h1>
       <div id={'memoContainer'} className={'flex flex-col items-center space-y-4 w-full h-full pt-4 overflow-y-scroll'}>
         {
           memos.map((memo: Memo) => {

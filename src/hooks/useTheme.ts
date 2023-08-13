96de5deb10e0ccb435609f849caa6227f05873f9
Timeuh <timeuuh@gmail.com>
2023-08-13 10:29:20 +0200

    refactor: use light theme by default and return the theme setter

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,20 +2,14 @@ import {useEffect, useState} from 'react';
 import {Theme} from '@/appTypes';
 
 const useTheme = () => {
-  const [theme, setTheme] = useState<Theme>('dark');
+  const [theme, setTheme] = useState<Theme>('light');
   const [isCurrentLight, setIsCurrentLight] = useState<boolean>(true);
-
-  const toggleTheme = () => {
-    setTheme(prevTheme => {
-      return (prevTheme === 'light' ? 'dark' : 'light');
-    });
-  };
 
   useEffect(() => {
     setIsCurrentLight(theme === 'light');
   }, [theme]);
 
-  return {isCurrentLight, toggleTheme};
+  return {isCurrentLight, setTheme};
 };
 
 export default useTheme;

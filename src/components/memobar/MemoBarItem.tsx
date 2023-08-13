eda4380b786f2d62928a88d726b5fadc5f538bce
Timeuh <timeuuh@gmail.com>
2023-08-13 10:09:18 +0200

    style: use theme for memo bar item

--- src/components/memobar/MemoBarItem.tsx
+++ src/components/memobar/MemoBarItem.tsx
@@ -1,4 +1,5 @@
 import {Memo} from '@/appTypes';
+import useTheme from '@/hooks/useTheme';
 
 type Props = {
   memo: Memo,
@@ -6,10 +7,13 @@ type Props = {
 }
 
 export default function MemoBarItem({memo, selectMemo}: Props) {
+  const {isCurrentLight} = useTheme();
+
   return (
-    <div className={'memo-item-desktop'} onClick={() => {
-      selectMemo(memo);
-    }}>
+    <div className={`memo-item-desktop ${isCurrentLight ? 'border-dark hover:bg-purple hover:text-light' : 'border-light hover:bg-dark text-light'}`}
+      onClick={() => {
+        selectMemo(memo);
+      }}>
       <h3 className={'text-xl'}>{memo.title}</h3>
     </div>
   );

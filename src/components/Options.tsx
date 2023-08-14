21751eb7870d467785f41517ee19fd8d149c3a75
Timeuh <timeuuh@gmail.com>
2023-08-14 10:06:38 +0200

    feat: add langage change menu

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,18 +1,32 @@
 import {useTheme} from '@/hooks/useTheme';
+import light from '@/img/light.png';
+import dark from '@/img/dark.png';
+import french from '@/img/france.png';
+import english from '@/img/england.png';
+import {useLang} from '@/hooks/useLang';
 
 export default function Options() {
   const {isCurrentLight, setTheme} = useTheme();
+  const {setLang} = useLang();
 
   return (
-    <div id={'options'} className={`absolute w-1/6 h-24 -top-10 left-10 rounded-md shadow-lg hidden xl:block
+    <div id={'options'} className={`absolute w-1/6 h-fit -top-16 left-10 rounded-md shadow-lg hidden xl:block py-2 space-y-2
       ${isCurrentLight ? 'shadow-main bg-violet' : 'shadow-purple bg-purple'}`}>
       <div className={'h-full flex flex-row items-center justify-center space-x-8'}>
         <button onClick={() => {
           setTheme('light');
-        }}><img src='src/img/light.png' alt='light mode'/></button>
+        }}><img src={light} alt='light mode'/></button>
         <button onClick={() => {
           setTheme('dark');
-        }}><img src='src/img/dark.png' alt='dark mode'/></button>
+        }}><img src={dark} alt='dark mode'/></button>
+      </div>
+      <div className={'h-full flex flex-row items-center justify-center space-x-8'}>
+        <button onClick={() => {
+          setLang('fr');
+        }}><img src={french} alt='french langage' className={'h-16 w-16'}/></button>
+        <button onClick={() => {
+          setLang('en');
+        }}><img src={english} alt='english langage' className={'h-16 w-16'}/></button>
       </div>
     </div>
   );
--- src/components/OptionsMenuMobile.tsx
+++ src/components/OptionsMenuMobile.tsx
@@ -3,11 +3,15 @@ import optionsLight from '@/img/options-light.png';
 import optionsDark from '@/img/options-dark.png';
 import lightMode from '@/img/light.png';
 import darkMode from '@/img/dark.png';
+import french from '@/img/france.png';
+import english from '@/img/england.png';
 import {useState} from 'react';
+import {useLang} from '@/hooks/useLang';
 
 export default function OptionsMenuMobile() {
   const {isCurrentLight, setTheme} = useTheme();
   const [isActive, setIsActive] = useState<boolean>(false);
+  const {setLang} = useLang();
 
   return (
     <div id={'optionsMobile'} className={'w-full xl:hidden overflow-hidden'}>
@@ -17,16 +21,26 @@ export default function OptionsMenuMobile() {
       }}>
         <img src={isCurrentLight ? optionsLight : optionsDark} alt='options logo' className={'w-8 h-8'}/>
       </button>
-      <div className={`w-1/3 h-1/5 rounded-md absolute top-20 right-4 flex flex-row items-center justify-center transition-transform duration-500
+      <div className={`w-1/3 h-1/5 rounded-md absolute top-20 right-4 flex flex-col items-center justify-center transition-transform duration-500
           ${isCurrentLight ? 'bg-violet' : 'bg-purple'} ${isActive ? 'translate-x-0' : 'translate-x-48'}`} onClick={() => {
         setIsActive(!isActive);
       }}>
-        <button onClick={() => {
-          setTheme('light');
-        }}><img src={lightMode} alt='light mode'/></button>
-        <button onClick={() => {
-          setTheme('dark');
-        }}><img src={darkMode} alt='dark mode'/></button>
+        <div className={'w-full h-1/2 flex flex-row items-center justify-center'}>
+          <button onClick={() => {
+            setTheme('light');
+          }}><img src={lightMode} alt='light mode'/></button>
+          <button onClick={() => {
+            setTheme('dark');
+          }}><img src={darkMode} alt='dark mode'/></button>
+        </div>
+        <div className={'w-full h-1/2 flex flex-row items-center justify-center'}>
+          <button onClick={() => {
+            setLang('fr');
+          }}><img src={french} alt='french lagage' className={'h-16 w-16'}/></button>
+          <button onClick={() => {
+            setLang('en');
+          }}><img src={english} alt='enflish langage' className={'h-16 w-16'}/></button>
+        </div>
       </div>
     </div>
   );

10a05a3eba1e6e7b7a6f7bb896c2e651e4fa3035
Timeuh <timeuuh@gmail.com>
2023-08-14 09:19:54 +0200

    feat: use lang context

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import {ThemeProvider} from '@/hooks/useTheme';
+import {LangProvider} from '@/hooks/useLang';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <LangProvider>
+        <App />
+      </LangProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
